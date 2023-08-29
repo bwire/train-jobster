f01f35ca3880128b946f4f58586185832c0e0ccc
barbedwire <e.barbedwire@gmail.com>
2023-08-29 10:20:17 +0200

    change landing text

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -52,7 +52,14 @@ const Landing = () => {
           <h1>
             job <span>tracking</span> app
           </h1>
-          <p>some text</p>
+          <p>
+            I'm baby affogato cronut gochujang selfies, tofu slow-carb glossier
+            tilde enamel pin butcher. DIY next level gluten-free, fashion axe
+            cliche polaroid hammock hot chicken prism raw denim. Tousled af
+            shaman dreamcatcher plaid, street art ugh readymade semiotics chia
+            XOXO gastropub selvage tofu. Before they sold out williamsburg twee
+            subway tile.
+          </p>
           <button className='btn btn-hero'>Login/Register</button>
         </div>
         <img src={main} alt='job hunt' className='img main-img' />
