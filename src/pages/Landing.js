8b7b0d47796007da46454f5fea0fa462d99404fa
barbedwire <e.barbedwire@gmail.com>
2023-09-02 19:27:04 +0200

    make use of predefined wrappers

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,45 +1,7 @@
 import React from 'react';
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
-import styled from 'styled-components';
-
-const Wrapper = styled.main`
-  h1: {
-    font-weight: 700;
-    span: {
-      color: var(--primary-color);
-    }
-  }
-  p: {
-    color: var(--grey-600);
-  }
-  nav: {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc() (100vh - var() (--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-  .main-img {
-    display: none;
-  }
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`;
+import Wrapper from '../assets/wrappers/LandingPage';
 
 const Landing = () => {
   return (
