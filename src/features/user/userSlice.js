887333660352000f4bf67685f0f855d2d9e0db35
barbedwire <e.barbedwire@gmail.com>
2023-09-11 18:32:20 +0200

    add extra reducers

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,22 +1,70 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import axios from '../../utils/axios';
 
 const initialState = {
   isLoading: true,
   user: null,
 };
 
-export const registerUser = createAsyncThunk('user/register', (user) => {
-  console.log('User created', user);
-});
+export const registerUser = createAsyncThunk(
+  'user/registerUser',
+  async (user, api) => {
+    try {
+      const resp = await axios.post('/auth/register', user);
+      return resp.data;
+    } catch (error) {
+      return api.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
 
-export const loginUser = createAsyncThunk('user/login', (user) => {
-  console.log('User logged in', user);
-});
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (user, api) => {
+    try {
+      const resp = await axios.post('/auth/login', user);
+      return resp.data;
+    } catch (error) {
+      return api.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+        toast.success(`Hello There ${user.name}`);
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        console.log('fucking rejected', payload);
+        state.isLoading = false;
+        toast.error(payload);
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+        toast.success(`Welcome Back ${user.name}`);
+      })
+      .addCase(loginUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export default userSlice.reducer;
