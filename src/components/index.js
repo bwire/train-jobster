a7f8e5e8937ec78da07edd5dda79036debfafc74
barbedwire <e.barbedwire@gmail.com>
2023-09-12 05:59:50 +0200

    add draft layout

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,8 @@ import Logo from './Logo';
 import Error from './Error';
 import Register from './Register';
 import FormRow from './FormRow';
+import Navbar from './Navbar';
+import SmallSidebar from './SmallSidebar';
+import BigSidebar from './BigSidebar';
 
-export { Logo, Error, Register, FormRow };
+export { Logo, Error, Register, FormRow, Navbar, BigSidebar, SmallSidebar };
