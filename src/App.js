4ed82a46063c29dead78445c624e4b49cbe0d840
barbedwire <e.barbedwire@gmail.com>
2023-09-12 05:35:43 +0200

    add main dashboard components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Landing from './pages/Landing';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Dashboard, Register, Error } from './components';
-import 'react-toastify/dist/ReactToastify.css';
+import { Register, Error } from './components';
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from './pages/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job ' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path='landing' element={<Landing />} />
         <Route path='register' element={<Register />} />
         <Route path='*' element={<Error />} />
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import Logo from './Logo';
 import Error from './Error';
-import Dashboard from './Dashboard';
 import Register from './Register';
 import FormRow from './FormRow';
 
-export { Logo, Error, Dashboard, Register, FormRow };
+export { Logo, Error, Register, FormRow };
