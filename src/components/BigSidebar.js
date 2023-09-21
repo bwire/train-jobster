b3ba55d562abb55f20575141eda04227df5fd12d
barbedwire <e.barbedwire@gmail.com>
2023-09-21 18:23:20 +0200

    add BidSidebar bits

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => state.user);
   return (
     <Wrapper>
-      <div>BigSidebar</div>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
