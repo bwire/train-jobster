b6e2f6d8951bfee6092f768de9f8aed4df58a444
barbedwire <e.barbedwire@gmail.com>
2023-10-15 20:59:31 +0200

    fix FormRow component

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const FormRow = ({ type, name, value, onChange, labelText }) => {
+const FormRow = ({ type, name, value, handleChange, labelText }) => {
   return (
-    <div className="form-row">
-      <label htmlFor={name} className="form-label">
+    <div className='form-row'>
+      <label htmlFor={name} className='form-label'>
         {labelText || name}
       </label>
       <input
         type={type}
-        className="form-input"
+        className='form-input'
         name={name}
         value={value}
-        onChange={onChange}
+        onChange={handleChange}
       />
     </div>
   );
